b3df3d7b92fa5dfe26aeba85dab2974d9e607c55
Rafsan Bhuiyan <48545950+rafsanbhuiyan@users.noreply.github.com>
2020-04-24 09:40:07 -1000

    Create main.go

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+//Rafsan Bhuiyan
+
+
+//log to handle errors, net/http for restful Api
+import (
+    "log"
+    "net/http"
+
+    "https://github.com/rafsanbhuiyan/myGoCode.git"
+)
+
+//Creating get function
+func get(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(`{"message": "get called"}`))
+}
+
+//creating a post function
+func post(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    w.Write([]byte(`{"message": "post called"}`))
+}
+
+//creating a put function
+func put(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusAccepted)
+    w.Write([]byte(`{"message": "put called"}`))
+}
+
+//Delete function
+func delete(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(`{"message": "delete called"}`))
+}
+
+//function not found
+func notFound(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusNotFound)
+    w.Write([]byte(`{"message": "not found"}`))
+}
+
+//Main function and initiating the objects of the methods
+func main() {
+    r := mux.NewRouter()
+    r.HandleFunc("/", get).Methods(http.MethodGet)
+    r.HandleFunc("/", post).Methods(http.MethodPost)
+    r.HandleFunc("/", put).Methods(http.MethodPut)
+    r.HandleFunc("/", delete).Methods(http.MethodDelete)
+    r.HandleFunc("/", notFound)
+    log.Fatal(http.ListenAndServe(":8080", r))
+}
